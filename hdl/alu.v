dd8a0c2f69fd1d3f492c9ab872718f88c28284f8
Jesse Taube <Mr.Bossman075@gmail.com>
2023-01-17 17:17:28 -0500

    alu is done need to fix jumps

---
+++ hdl/alu.v
@@ -0,0 +1,47 @@
+module alu
+	(
+	input [3:0]m,
+	input [31:0]a, b,
+	output reg [31:0]y
+	);
+	// zf: zero
+	// cf: carry out, WIDTH bit
+	// of: overflow
+	// sf: sign, WIDTH-1
+
+	wire signed [31:0]a_signed = a;
+	wire [31:0]addition = a + b;
+	wire [32:0]subtraction = a - b;
+	wire sub_of = (!a[31] & b[31] & subtraction[31]) |
+				 (a[31] & !b[31] & !subtraction[31]);
+	wire sub_zf = (subtraction[31:0] == 32'h0);
+	wire sub_sf = subtraction[31];
+	wire sub_cf = subtraction[32];
+	always @ (*) begin
+		case(m)
+			4'b0000: // add
+				y = addition[31:0];
+			4'b1000: // sub
+				y = subtraction[31:0];
+			4'b0001: // sll
+				y = a << b[4:0];
+			4'b0010: // slt
+				y = {31'b0, (sub_of ^ sub_sf) & !sub_zf};
+			4'b0011: // sltu
+				y = {31'b0, sub_cf};
+			4'b0100: // xor
+				y = a ^ b;
+			4'b0101: // srl
+				y = a >> b[4:0];
+			4'b1101: // sra
+				y = a_signed >>> b[4:0];
+			4'b0110: // or
+				y = a | b;
+			4'b0111: // and
+				y = a & b;
+			default: begin // error
+				y = 32'hDEADBEEF;
+			end
+		endcase
+	end
+endmodule
--- hdl/prog_counter.v
+++ hdl/prog_counter.v
@@ -1,38 +1,56 @@
 /* verilator lint_off UNUSED */
 /* verilator lint_off WIDTH */
-
+/* TODO
+JAL
+JALR
+BEQ
+BNE
+BLT
+BGE
+BLTU
+BGEU
+LBU
+LHU
+*/
 module prog_counter
 	(input clk,
 	input rts,
 	output [31:0]prc,output [31:0]odat);
 	reg [31:0] pc;
-	reg [31:0] opc;
 	reg [31:0] regs[32];
 	reg [31:0] instruction;
-	reg [31:0] temp = 1000;
 	reg [1:0] microop = 0;
 	reg [1:0] dsize = 3;
 	wire [31:0] data;
 	wire [31:0] addr;
+	wire [31:0] alu_out;
 	reg rwr = 1;
 	wire cs = 0;
 	wire oe = 0;
-	assign prc = (microop==0)?pc:opc;
-	assign addr = (microop==0)?pc:(regs[rs1]+((instruction[5])?imm_s:imm_i));
-	assign data = (microop==2)?regs[rs2]:'hz;
+	assign addr = (microop==0)?pc:(regs[mo_rs1]+((instruction[5])?mo_imm_s:mo_imm_i));
+	assign data = (microop==2)?regs[mo_rs2]:'hz;
 	sram ram(rwr,oe,cs,addr,dsize,data);
-	assign odat = data;
+	wire ex_alu = (sub_op == 5 || op == 7'b0010011)?data[30]:0; // use extra ops for SRAI/SRA and non-imm(sub/add)
+	alu alu(sub_op | (ex_alu << 4),regs[rs1],(instruction[5])?regs[rs2]:imm_i,alu_out);
 
-	wire [6:0] op = instruction[6:0];
+	reg [31:0] opc;
+	assign odat = data;
+	assign prc = (microop==0)?pc:opc;
+
+	wire [4:0] mo_rs1 = instruction[19:15];
+	wire [4:0] mo_rs2 = instruction[24:20];
+	wire [31:0] mo_imm_s = {{21{instruction[31]}}, instruction[30:25], instruction[11:7]};
+	wire [31:0] mo_imm_i = {{21{instruction[31]}}, instruction[30:20]};
+
+	wire [6:0] op = data[6:0];
+	wire [4:0] rs1 = data[19:15];
+	wire [4:0] rs2 = data[24:20];
+	wire [4:0] rd = data[11:7];
 	wire [2:0] sub_op = data[14:12];
-	wire [4:0] rs1 = instruction[19:15];
-	wire [4:0] rs2 = instruction[24:20];
-	wire [4:0] rd = instruction[11:7];
-	wire [31:0] imm_i = {{21{instruction[31]}}, instruction[30:20]};
-	wire [31:0] imm_b = {{20{instruction[31]}}, instruction[7], instruction[30:25], instruction[11:8], 1'b0};
-	wire [31:0] imm_j = {{12{instruction[31]}}, instruction[19:12], instruction[20], instruction[30:21], 1'b0};
-	wire [31:0] imm_u = {instruction[31:12], 12'b0};
-	wire [31:0] imm_s = {{21{instruction[31]}}, instruction[30:25], instruction[11:7]};
+	wire [31:0] imm_i = {{21{data[31]}}, data[30:20]};
+	wire [31:0] imm_b = {{20{data[31]}}, data[7], data[30:25], data[11:8], 1'b0};
+	wire [31:0] imm_j = {{12{data[31]}}, data[19:12], data[20], data[30:21], 1'b0};
+	wire [31:0] imm_u = {data[31:12], 12'b0};
 
 	always @(posedge clk or posedge rts or negedge clk) begin
 		if(rts)
@@ -42,15 +60,31 @@ module prog_counter
 				opc <= pc;
 				pc <= pc + 1;
 				instruction <= data;
-				case (data[6:0])
-					7'b0000011: begin
+				case (op)
+					7'b0000011: begin // LOAD
 						microop <= 1;
 						dsize <= (sub_op==2)?3:sub_op;
 					end
-					7'b0100011: begin
+					7'b0100011: begin // STORE
 						rwr <= 0;
 						microop <= 2;
 						dsize <= (sub_op==2)?3:sub_op;
+					end
+					7'b0010011: begin // ALUI
+						regs[rd] <= alu_out;
+					end
+					7'b0110011: begin // ALU
+						regs[rd] <= alu_out;
+					end
+					7'b0110111: begin // LUI
+						regs[rd] <= imm_u;
+					end
+					7'b0010111: begin // AUIPC
+						regs[rd] <= imm_u + (pc-1); // fixme
+					end
+					7'b1100111: begin // JALR
+						pc <= {imm_i + regs[rs1][31:1], 1'b0}; // fixme
+						regs[rd] <=  {imm_i + regs[rs1][31:1], 1'b0} + 4;
 					end
 					default:;
 				endcase
