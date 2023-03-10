1fe4edb5235ae2700cac7854acbf7926606645d0
Jesse Taube <Mr.Bossman075@gmail.com>
2023-03-10 12:00:25 -0500

    back to uart as example

--- src/rv32.s
+++ src/rv32.s
@@ -4,36 +4,25 @@
 .align 4
 start:
 	la x10, 0x10000000
-	li x1, 1
-	li x2, 2
-	li x3, 3
-	li x4, 4
-	li x6, 6
-	li x7, 7
-	la x5, test
-	csrrw x0, 0x305, x5
-	la x8, 0x80000
-	lw x9,0(x8)
-	.word 0x1;
-2:
 	la x6, .texts
-	ecall
+	li x5,0x80
+	sb x5, (4*3)(x10)
+	li x5, 8
+	sb x5,0(x10)
+	li x5,3
+	sb x5, (4*3)(x10)
 1:
-	lw x5, 0(x6)
+	lb x5, 0(x6)
 	sb x5, 0(x10)
+	li x4,0xffff
+2:
+	addi x4,x4,-1
+	bnez x4, 2b
 	addi x6,x6,1
 	bnez x5,1b
-	.word 0x00;
-
-test:
-	li x5, 'a'
-	sb x5, 0(x10)
-	csrrs x5, mepc, x0
-	addi x5,x5,4
-	csrrw x0, mepc, x5
-	mret
+	j start
 
 .texts:
-	.ascii "test\n";
+	.ascii "test\n\r";
 	.byte 0x00;
 	.balign 4, 0;
