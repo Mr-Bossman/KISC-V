38e63671c7ff71b6d89781d4c14b29ee0bc80986
Jesse Taube <Mr.Bossman075@gmail.com>
2024-04-26 19:02:52 -0400

    update gen_microcode program

--- src/gen_microcode.py
+++ src/gen_microcode.py
@@ -62,9 +62,21 @@ JALR = [Micro_ops.LOAD_PC,
 BRANCH = [Micro_ops.ALU_FLAGS | Micro_ops.LOAD_PC,
 	  Micro_ops.NOP]
 
+def print_err(*args, **kwargs):
+	print(*args, file=sys.stderr, **kwargs)
+
+def help():
+	print_err("Usage: ", sys.argv[0], "[-n] [-h] [output file]]")
+	print_err("Options:")
+	print_err("-h: Prints this help message")
+	print_err("-n: Generate microcode without the enable signal")
+	print_err("default output file: microcode.hex")
+	print_err("default output file with [-n]: microcode_no_en.hex")
+
 def gen_opcodes(index, microcode,loadins):
+	ret = ""
 	if(len(microcode) > 16-len(loadins)):
-		print("Too many microcode instructions")
+		print_err("Too many microcode instructions")
 		return
 	ofs = 1 if index == 0 else len(loadins)
 	for i in range(16):
@@ -77,56 +89,67 @@ def gen_opcodes(index, microcode,loadins):
 			# end jump to start
 			if(i == len(microcode)-1):
 				code = microcode[i]
-		print("{:04x}".format(code))
+		ret += "{:04x}\n".format(code)
 	if index == 0:
 		for _ in range(len(loadins)-1):
-			print("0"*4)
-def gen_microcode():
-	gen_opcodes(0, LOADINS, LOADINS)
-	gen_opcodes(1, STORE, LOADINS)
-	gen_opcodes(2, LOAD, LOADINS)
-	gen_opcodes(3, SYSTEM, LOADINS)
-	gen_opcodes(4, ALU, LOADINS)
-	gen_opcodes(5, JALR, LOADINS)
-	gen_opcodes(6, BRANCH, LOADINS)
+			ret += "0"*4 + "\n"
+	return ret
+
+def gen_microcode(file_name="microop.hex"):
+	hex_str = ""
+	file = open(file_name, "w")
+	hex_str += gen_opcodes(0, LOADINS, LOADINS)
+	hex_str += gen_opcodes(1, STORE, LOADINS)
+	hex_str += gen_opcodes(2, LOAD, LOADINS)
+	hex_str += gen_opcodes(3, SYSTEM, LOADINS)
+	hex_str += gen_opcodes(4, ALU, LOADINS)
+	hex_str += gen_opcodes(5, JALR, LOADINS)
+	hex_str += gen_opcodes(6, BRANCH, LOADINS)
 	# pad out the rest of the microcode
 	# this allows for 7 opcodes
 	# the last opcode is a jump to the start
 	for _ in range(16-(len(LOADINS)-1)):
-		print("0"*4)
-	#for i in range(9):
-	#	print(i*8)
+		hex_str += "0"*4 + "\n"
 
-def gen_microcode_no_en():
-	gen_opcodes(0, LOADINS_NO_EN, LOADINS_NO_EN)
-	gen_opcodes(1, STORE_NO_EN, LOADINS_NO_EN)
-	gen_opcodes(2, LOAD_NO_EN, LOADINS_NO_EN)
-	gen_opcodes(3, SYSTEM_NO_EN, LOADINS_NO_EN)
-	gen_opcodes(4, ALU, LOADINS_NO_EN)
-	gen_opcodes(5, JALR, LOADINS_NO_EN)
-	gen_opcodes(6, BRANCH, LOADINS_NO_EN)
+	file.write(hex_str)
+	file.close()
+
+
+def gen_microcode_no_en(file_name="microop_no_en.hex"):
+	hex_str = ""
+	file = open(file_name, "w")
+	hex_str += gen_opcodes(0, LOADINS_NO_EN, LOADINS_NO_EN)
+	hex_str += gen_opcodes(1, STORE_NO_EN, LOADINS_NO_EN)
+	hex_str += gen_opcodes(2, LOAD_NO_EN, LOADINS_NO_EN)
+	hex_str += gen_opcodes(3, SYSTEM_NO_EN, LOADINS_NO_EN)
+	hex_str += gen_opcodes(4, ALU, LOADINS_NO_EN)
+	hex_str += gen_opcodes(5, JALR, LOADINS_NO_EN)
+	hex_str += gen_opcodes(6, BRANCH, LOADINS_NO_EN)
 	# pad out the rest of the microcode
 	# this allows for 7 opcodes
 	# the last opcode is a jump to the start
-	for _ in range(16-(len(LOADINS)-1)):
-		print("0"*4)
-	#for i in range(9):
-	#	print(i*8)
+	for _ in range(16-(len(LOADINS_NO_EN)-1)):
+		hex_str += ("0"*4) + "\n"
 
-def print_err(*args, **kwargs):
-	print(*args, file=sys.stderr, **kwargs)
+	file.write(hex_str)
+	file.close()
 
-def help():
-	print_err("Usage: ", sys.argv[0], "[-n] > microcode.hex")
-	print_err("Options:")
-	print_err("-n: Generate microcode without the enable signal")
 
 def main():
-	if len(sys.argv) == 2:
+	if len(sys.argv) == 3:
 		if sys.argv[1] == "-n":
-			gen_microcode_no_en()
+			gen_microcode_no_en(sys.argv[2])
+		elif sys.argv[2] == "-n":
+			gen_microcode_no_en(sys.argv[1])
 		else:
 			help()
+	elif len(sys.argv) == 2:
+		if sys.argv[1] == "-n":
+			gen_microcode_no_en()
+		elif sys.argv[1][0] == "-":
+			help()
+		else:
+			gen_microcode(sys.argv[1])
 	elif len(sys.argv) == 1:
 		gen_microcode()
 	else:
